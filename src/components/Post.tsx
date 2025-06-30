e30dced1974e34e09a9ebf74332788fe5100aa16
aramind <88814622+aramind@users.noreply.github.com>
2025-06-30 22:59:38 +0800

    feat: styling comments

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Comments from "./Comments";
 
 const Post = () => {
   return (
@@ -36,7 +37,7 @@ const Post = () => {
         </p>
       </div>
       {/* INTERACTION */}
-      <div className="flex items-center justify-between text-sm">
+      <div className="flex items-center justify-between text-sm my-4">
         <div className="flex gap-8">
           <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
             <Image
@@ -81,6 +82,7 @@ const Post = () => {
           </div>
         </div>
       </div>
+      <Comments />
     </div>
   );
 };
