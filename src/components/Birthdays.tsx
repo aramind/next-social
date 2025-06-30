28a764e5e1caed2089950f6284ffb67b33e59501
aramind <88814622+aramind@users.noreply.github.com>
2025-06-30 23:58:15 +0800

    feat: styling Birthdays section

--- src/components/Birthdays.tsx
+++ src/components/Birthdays.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Birthdays = () => {
@@ -23,6 +24,22 @@ const Birthdays = () => {
             Celebrate
           </button>
         </div>
+      </div>
+      {/* UPCOMING */}
+      <div className="p-4 bg-slate-100 rounded-lg flex items-center gap-4">
+        <Image
+          src="/gift.png"
+          alt=""
+          width={40}
+          height={40}
+          className="w-10 h-10 rounded-full object-cover"
+        />
+        <Link href="/" className="flex flex-col gap-1 text-xs">
+          <span className="text-gray-700 font-semibold">
+            Upcoming Birthdays
+          </span>
+          <span>See other 16 have upcoming birthdays</span>
+        </Link>
       </div>
     </div>
   );
