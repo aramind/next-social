04cd90d0c550475ec78848b4b029b0acb5e727ce
aramind <88814622+aramind@users.noreply.github.com>
2025-06-30 09:40:25 +0800

    feat: changing the hamburger button on click

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -3,18 +3,29 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 const MobileMenu = () => {
-  const [isOpen, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
   return (
     <div>
       <div
         className="flex flex-col gap-1 cursor-pointer"
-        onClick={() => setOpen((prev) => !prev)}
+        onClick={() => setIsOpen((prev) => !prev)}
       >
         <div
-          className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen} ? "rotate-45" : ""`}
-        ></div>
-        <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
-        <div className="w-6 h-1 bg-blue-500 rounded-sm"></div>
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "rotate-45" : ""
+          } origin-left`}
+        />
+        <div
+          className={`"w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "opacity-0" : ""
+          }`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "-rotate-45" : ""
+          } origin-left`}
+        />
       </div>
       {isOpen && (
         <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
