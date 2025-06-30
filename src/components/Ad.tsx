158a200ef8e00e308779bfc79b7d50c8d556902a
aramind <88814622+aramind@users.noreply.github.com>
2025-07-01 00:31:59 +0800

    feat: styling ad section

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,7 +1,53 @@
+import Image from "next/image";
 import React from "react";
 
 const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
-  return <div className="p-4 bg-white rounded-lg shadow-md text-sm">Ad</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm">
+      {/* TOP */}
+      <div className="flex items-center justify-between text-gray-500 font-medium">
+        <span>Sponsored Ads</span>
+        <Image src="/more.png" alt="" width={16} height={16} />
+      </div>
+      {/* BOTTOM */}
+      <div
+        className={`flex flex-col mt-4 ${size === "sm" ? "gap-2" : "gap-4"}`}
+      >
+        <div
+          className={`relative w-full ${
+            size === "sm" ? "h-24" : size === "md" ? "h-36" : "h-48"
+          }`}
+        >
+          <Image
+            src="https://images.pexels.com/photos/32650223/pexels-photo-32650223.jpeg"
+            alt=""
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/32650223/pexels-photo-32650223.jpeg"
+            alt=""
+            width={24}
+            height={24}
+            className="rounded-full w-6 h-6 object-cover"
+          />
+          <span className="text-blue-500 font-medium">Bird Ecopark</span>
+        </div>
+        <p className={size === "sm" ? "text-xs" : "text-sm"}>
+          {size === "sm"
+            ? "Lorem ipsum dolor sit amet."
+            : size === "md"
+            ? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, officiis"
+            : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corrupti dicta repellendus reprehenderit tenetur sit, necessitatibus cum eveniet voluptatum? Dignissimos!"}
+        </p>
+        <button className="bg-gray-200 text-gray-500 p-2 text-xs rounded-lg">
+          Learn more
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Ad;
