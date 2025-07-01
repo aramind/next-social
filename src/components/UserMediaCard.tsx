811f931056a288dc08cfa6b7c7b8109a8c4d2885
aramind <88814622+aramind@users.noreply.github.com>
2025-07-01 09:00:40 +0800

    feat: styling the user media section

--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -10,6 +11,73 @@ const UserMediaCard = ({ userId }: { userId: string }) => {
         <Link href="/" className="text-blue-500 text-xs">
           See all
         </Link>
+      </div>
+      {/* BOTTOM */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1645920/pexels-photo-1645920.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/20183314/pexels-photo-20183314.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/20422500/pexels-photo-20422500.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/13911138/pexels-photo-13911138.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/20409358/pexels-photo-20409358.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/830829/pexels-photo-830829.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/31353383/pexels-photo-31353383.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/32542762/pexels-photo-32542762.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
       </div>
     </div>
   );
