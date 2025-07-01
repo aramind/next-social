bb4d221ca6088509f07227f6a72e02f55251d0fc
aramind <88814622+aramind@users.noreply.github.com>
2025-07-01 10:05:01 +0800

    feat: styling my profile card

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProfileCard from "./ProfileCard";
 import Link from "next/link";
 import Image from "next/image";
+import Ad from "./Ad";
 
 const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
@@ -87,8 +88,8 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
           <Image src="/settings.png" alt="" width={20} height={20} />
           <span>Settings</span>
         </Link>
-        <hr className="border-t-1 border-gray-50 w-36 sef-center" />
       </div>
+      <Ad size="sm" />
     </div>
   );
 };
